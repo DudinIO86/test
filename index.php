290e373b07f545ac3d6632c2953932c690b873a6
DudinIO <dudinio86@gmail.com>
2024-11-04 15:47:47 +0300

    gg

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+if (2 > 11) {
+    echo "true";
+} else {
+    echo "false";
+}
+echo "\n";
+if (1>2){
+    $x=1;
+} else{
+    $x=2;
+}
+
+$x=(1>2)? 1:2;
+
+echo $x;
+echo "\n";
+
+
+
+print_r ("hello");
